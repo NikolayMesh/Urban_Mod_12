9fb9b5edeb2b0e20b1a309cba6305353bf4b0b96
NikolayMesh <partizan.91@bk.ru>
2024-11-30 14:11:35 +0300

    add+

--- test_runner.py
+++ test_runner.py
@@ -4,18 +4,27 @@ import unittest
 
 class RunnerTest(unittest.TestCase):
     def test_walk(self):
+        """test_walk - метод, в котором создаётся объект класса Runner с произвольным именем.
+         Далее вызовите метод walk у этого объекта 10 раз. После чего методом assertEqual
+          сравните distance этого объекта со значением 50."""
         walker = runner.Runner("test walker")
         for _ in range(10):
             walker.walk()
         self.assertEquals(walker.distance, 50)
 
     def test_run(self):
+        """test_run - метод, в котором создаётся объект класса Runner с произвольным именем.
+         Далее вызовите метод run у этого объекта 10 раз. После чего методом assertEqual
+         сравните distance этого объекта со значением 100."""
         runner_ = runner.Runner("test runner")
         for _ in range(10):
             runner_.run()
         self.assertEquals(runner_.distance,100)
 
     def test_challenge(self):
+        """test_challenge - метод в котором создаются 2 объекта класса Runner с произвольными именами.
+         Далее 10 раз у объектов вызываются методы run и walk соответственно. Т.к. дистанции должны быть
+         разными, используйте метод assertNotEqual, чтобы убедится в неравенстве результатов."""
         runner1 = runner.Runner("test runner1")
         walker2 = runner.Runner("test walker2")
         for _ in range(10):
